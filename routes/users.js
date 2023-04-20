4d2114ff6f0ab8b3db93acf27894307d26c1ec26
Jayant Rohila <jrohila55@gmail.com>
2023-04-20 10:34:31 +0530

    removed try catch

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,9 @@
-const express = require('express')
-
-const userRouter = express.Router()
-
-const userController = require('../controllers/users')
+const express = require('express'),
+  userRouter = express.Router(),
+  userController = require('../controllers/users')
 
 userRouter.post('/login', userController.login)
 
 userRouter.post('/signup', userController.signup)
-
-// Catch-all error handler for the user router
-userRouter.use((err, req, res, next) => {
-  console.error(err)
-
-  res.status(500).json({ error: 'Internal Server Error' })
-})
-
+// need to use auth token for every api expect login
 module.exports = userRouter
--- services/common.utils.js
+++ services/common.utils.js
@@ -1,32 +1,16 @@
-// Export a function called "validateRequest" that takes two arguments: a "request" object and a "params" object
 module.exports.validateRequest = (request, params) => {
-  try {
-    let isInvalidRequest = Object.keys(request).some(
-      (key) => !Object.keys(params).includes(key),
-    )
-    // If there are any invalid keys, throw an Error to indicate an invalid request
-    if (isInvalidRequest) throw new Error('Invalid request')
+  let isInvalidRequest = Object.keys(request).some(
+    (key) => !Object.keys(params).includes(key),
+  )
+  if (isInvalidRequest) return isInvalidRequest
 
-    // Create an array of values that are considered "invalid" for request parameters
-    const invalidArray = [null, undefined, 'null', 'undefined', '']
-
-    // Loop through each key in the "params" object
-    for (const key in params) {
-      // If the value of the "params" key is not null/undefined and the corresponding value in the "request" object is considered invalid, set "isInvalidRequest" to true and break out of the loop
-      if (params[key] && invalidArray.includes(request[key])) {
-        isInvalidRequest = true
-        break
-      }
+  const invalidArray = [null, undefined, 'null', 'undefined', '']
+  for (const key in params) {
+    if (params[key] && invalidArray.includes(request[key])) {
+      isInvalidRequest = true
+      break
     }
-
-    if (isInvalidRequest) throw new Error('Invalid request')
-
-    // Return false to indicate a valid request
-    return false
-  } catch (error) {
-    console.error(error)
-
-    // Return true to indicate an invalid request
-    return true
   }
+  return isInvalidRequest
 }
+// function lke gen token,decypt password will be come in this utils file
