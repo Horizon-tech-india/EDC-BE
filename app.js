830916fc146c840309910219406b91f91591a2cd
Himanshu Jain <himanshujain@GS-5370.local>
2023-04-21 15:39:11 +0530

    fix : SSL cert for https server

--- app.js
+++ app.js
@@ -8,6 +8,21 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', true);
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin,X-Requested-With,Content-Type,Accept,Authorization'
+    );
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET,POST,PUT,DELETE,PATCH,OPTIONS'
+    );
+    next();
+});
+
 app.use('/users', usersRouter)
 
 // Define a route handler
