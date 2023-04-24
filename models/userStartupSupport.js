d48cb8e5d2c2292a33cff7047c72bcbc48980737
Himanshu Jain <himanshujain@GS-5370.local>
2023-04-24 20:18:04 +0530

    fix: user startup support

--- models/userStartupSupport.js
+++ models/userStartupSupport.js
@@ -10,22 +10,16 @@ const StartupSupportSchema = new mongoose.Schema({
   institute: { type: String, required: true },
   otherInstitute: { type: String, required: true },
   aadhar: {
-    type: Number,
-    required: true,
-    validate: {
-      validator: function (v) {
-        return v.toString().length === 12
-      },
-      message: (props) => `${props.value} is not a valid Aadhar number !`,
-    },
+    type: [Number, String],
+    required: false,
   },
   category: { type: String, required: true },
   categoryOther: { type: String, required: true },
   otherUniversity: { type: String, required: true },
   otherOrganisation: { type: String, required: true },
   designation: { type: String, required: true },
-  enrollmentNum: { type: Number, required: true },
-  teamSize: { type: Number, required: true },
+  enrollmentNum: { type: [Number, String], required: true },
+  teamSize: { type: [Number, String], required: true },
   teamMembers: { type: String, required: true },
   title: { type: String, required: true },
   uniqueFeatures: { type: String, required: true },
